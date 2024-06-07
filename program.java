5a90c4a64ac461058943417527f0b4adba82eaba
IzanamiiDevv <rafaeloli596@gmail.com>
2024-06-08 01:10:52 +0800

    Add Entry Point

--- program.java
+++ program.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 import src.logic;
 
 class Program {
     public static void main(String[] args) {
-        char[] moves = {'X',' ',' ',' ',' ',' ',' ',' ',' '};
+        Scanner req = new Scanner(System.in);
+        System.out.println("What is your Name?");
+        System.out.print("[+] ");
+        String name = req.nextLine();
+        System.out.println();
+
+        char player;
+        do {
+            player = req.next().charAt(0);
+        }while(!(player == 'X' || player == 'O'));
+        req.close();
+        char computer = player == 'X' ? 'O':'X';
+        char[] moves = {player,computer,' ',' ',' ',' ',' ',' ',' '};
         logic.generateTable(moves);
     }
 }
--- src/logic.java
+++ src/logic.java
@@ -4,15 +4,15 @@ public class logic {
     public static void generateTable(char[] moves) {
         System.out.println("+-------+-------+-------+");
         System.out.println("|       |       |       |");
-        System.out.println(String.format("|   %c   |   %c   |   %c   |",moves[0],moves[1],moves[3]));
+        System.out.println(String.format("|   %c   |   %c   |   %c   |",moves[0],moves[1],moves[2]));
         System.out.println("|       |       |       |");
         System.out.println("+-------+-------+-------+");
         System.out.println("|       |       |       |");
-        System.out.println("|   X   |   X   |   X   |");
+        System.out.println(String.format("|   %c   |   %c   |   %c   |",moves[3],moves[4],moves[5]));
         System.out.println("|       |       |       |");
         System.out.println("+-------+-------+-------+");
         System.out.println("|       |       |       |");
-        System.out.println("|   X   |   X   |   X   |");
+        System.out.println(String.format("|   %c   |   %c   |   %c   |",moves[6],moves[7],moves[8]));
         System.out.println("|       |       |       |");
         System.out.println("+-------+-------+-------+");
     }
