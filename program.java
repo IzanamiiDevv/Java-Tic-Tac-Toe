8f981bba419319a6081c2b5b3d3c0b8b7602c437
IzanamiiDevv <rafaeloli596@gmail.com>
2024-06-09 23:39:34 +0800

    Done

--- program.java
+++ program.java
@@ -1,17 +1,11 @@
 import java.util.Random;
 import java.util.Scanner;
-import src.Style;
 
 import src.logic;
 
 class Program extends logic {
     public static void main(String[] args) {
         Scanner req = new Scanner(System.in);
-        System.out.println("What is your Name?");
-        System.out.print("[+] ");
-        String name = req.nextLine();
-        System.out.println();
-
         char player;
         do {
             System.out.println("Choose Your Character (X/O)?");
